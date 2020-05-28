7508042fd362e9637bbc0287242ca9ff55fb0061
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-05-28 15:23:23 +0530

    newlsy added samples

---
+++ Mode.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { DropDownTreeComponent } from '@syncfusion/ej2-react-dropdowns';
+import * as dataSource from './dataSource.json';
+
+export default function App() {
+  let data = dataSource.defaultData as any;
+  let fields: any = { dataSource: data, value: 'id', text: 'name', child: 'subChild' };
+  return (
+    <div className="App">
+      <DropDownTreeComponent id='dropdowntree' fields={fields} showCheckBox={true} width={"350px"} mode={"Delimiter"} />
+    </div>
+  );
+}
